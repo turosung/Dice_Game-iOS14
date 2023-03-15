75799328634e787b86272335e3c41666a9b790fd
Nuhu Sulemana <turosung@gmail.com>
2023-03-15 22:45:38 +0000

    Built dice game by following Angela Udemy

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,36 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
-
-
-}
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        //        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour")
+        //        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
+        
+        // Created an array of dice which will be picked randomly for both dice
+        let diceArray = [
+            UIImage(imageLiteralResourceName: "DiceOne"),
+            UIImage(imageLiteralResourceName: "DiceTwo"),
+            UIImage(imageLiteralResourceName: "DiceThree"),
+            UIImage(imageLiteralResourceName: "DiceFour"),
+            UIImage(imageLiteralResourceName: "DiceFive"),
+            UIImage(imageLiteralResourceName: "DiceSix")
+        ]
+        
+        // We pick random dice and assign a different index based on the random numbers
+        diceImageView1.image = diceArray[(Int.random(in: 1...5))]
+            
+        diceImageView2.image = diceArray[(Int.random(in: 1...5))]
+                
+        }
+            
+    }
+        
 
